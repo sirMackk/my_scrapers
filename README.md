6da8755c484209663cb354265c7ebe1c5043eec6
sirMackk <c.tairix@gmail.com>
2012-12-24 16:41:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## A Sample Web Scraper
+
+### What is all this about?
+
+This is an example code snippet of a web scraper written in Python using the lxml library. It gets a webpage using the requests module, parses it using the lxml library, then dumps the data into a CSV file using the csv module. It can be much improved upon.
+
+This exploration of data analysis was sparked by http://schoolofdata.org/ and I'm thinking up ways of how awesome this is going to be when used with Django.
+
+### License
+
+Copyright (C) 2012  sirMackk
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
---
+++ scraper_example.py
@@ -0,0 +1,46 @@
+import requests
+import lxml.html
+import csv
+
+#TODO:
+#- figure out using xpath instead of cssselect
+#- figure out using something else instead of requests
+#- work on better csv output &&|| on sqlite output
+
+#url of file
+url = 'http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm'
+#gets html
+page = requests.get(url)
+
+#lxml uses html to create something, figure this out more
+root = lxml.html.fromstring(page.content)
+main_data = []
+#lxml uses cssselect to get all table rows marked by tr
+main_table = root.cssselect('tr')
+#then iterate through that list and use cssselect to get td columns
+for tr in main_table:
+    tds = tr.cssselect('td')
+#only append rows with 12 columns
+    if len(tds) == 12:
+#append a dictionary with data from columns to list
+        main_data.append({'country':tds[0].text_content(), 
+    'year':tds[1].text_content(), 
+    'total':tds[4].text_content(), 
+'men':tds[7].text_content(), 
+'women':tds[10].text_content()})
+
+
+print len(main_data)
+#creates csv writer object called countries.csv
+writer = csv.writer(open('countries.csv', 'wb'))
+#writes the first row of the csv with the headers
+writer.writerow(['Country', 'Year', 'Total', 'Men', 'Women'])
+#iterates through data
+for i in main_data:
+#j becomes a list of all items for each dict object in the list
+    j = i.items()
+#csv writer writes rows based on previously created list
+    writer.writerow([j[0][1].encode("utf8"), j[4][1], j[2][1], j[3][1], j[1][1]])
+    
+
+
