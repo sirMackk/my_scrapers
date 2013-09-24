2b88d1c68b17cb412dcbfafd856f5291631eed75
sirMackk <c.tairix@gmail.com>
2013-09-24 16:21:49 +0100

    Dat printf syntax. Dat threading

--- startup_locations.py
+++ startup_locations.py
@@ -1,7 +1,44 @@
-# scraper for startups wiki in ireland
-
 import requests
 from lxml import html
+import threading
+import Queue
+
+class Worker(threading.Thread):
+
+    def __init__(self, queue, root):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.root = root
+
+    def run(self):
+        global info
+        while 1:
+            try:
+                url = self.root + self.queue.get()
+                print url + '\n'
+                r = requests.get(url)
+                tree = html.fromstring(r.text)
+
+                tmp = []
+                #name
+                tmp.append(tree.xpath('//h1[@id="firstHeading"]/span/text()'))
+                #url
+                tmp.append(tree.xpath('//div[@id="mw-content-text"]/table[1]//tr[3]/td/a/text()'))
+                #location
+                tmp.append(tree.xpath('//div[@id="mw-content-text"]/table[1]//tr[4]/td/text()'))
+                #description
+                tmp.append(tree.xpath('//div[@id="mw-content-text"]/h3[3]/following::p[1]/text()'))
+
+                tmp = ['-' if not i else i for i in[j[0].strip().encode('utf-8').replace(',', ' ') if j else '-' for j in tmp]]
+                #this isnt needed since appending to lists cant invoke race conditions
+                #but it's really cool
+                lock.acquire()
+                info.append(tmp)
+                lock.release()
+                self.queue.task_done()
+            except Queue.Empty:
+                break
+
 root_url = 'http://startupwiki.ie'
 url = 'http://startupwiki.ie/wiki/Category:Startups'
 
@@ -9,26 +46,28 @@ index = requests.get(url)
 
 tree = html.fromstring(index.text)
 main_index = tree.xpath('//div[@class="mw-content-ltr"]//ul/li/a/attribute::href')
-print len(main_index)
-print main_index[0]
+print "Startups: %d" % len(main_index)
 info = []
+lock = threading.Lock()
 
-for i in main_index:
-    r = requests.get(root_url + i)
-    tree = html.fromstring(r.text)
-    print i
+queue = Queue.Queue()
 
-    name = tree.xpath('//h1[@id="firstHeading"]/span/text()')
-    url = tree.xpath('//div[@id="mw-content-text"]/table[1]//tr[3]/td/a/text()')
-    location = tree.xpath('//div[@id="mw-content-text"]/table[1]//tr[4]/td/text()')
-    desc = tree.xpath('//div[@id="mw-content-text"]/h3[3]/following::p[1]/text()')
+for i in xrange(7):
+    bee = Worker(queue, root_url)
+    bee.setDaemon(True)
+    bee.start()
 
-    info.append([name, url, location, desc])
+for startup in main_index:
+    queue.put(startup)
+
+queue.join()
+
+
 
 f = open('startups.csv', 'wb')
 for i in info:
     try:
-        f.write('%s,%s,%s,%s\n', i[0], i[1], i[2], [3])
+        f.write('%s,%s,%s,%s\n' % (i[0], i[1], i[2], i[3]))
     except IOError:
         print 'dat error in io'
 if f:
